b762fc046be3151fee447f549753a12feadefe06
blaubird <evereinenterprise@gmail.com>
2025-05-08 14:51:17 +0200

    Update main.py
    first steps/setteing up /health

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/health")
+async def health():
+    return {"ok": True}
+
+# временный корневой маршрут, чтобы увидеть «Hello»
 @app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+async def index():
+    return {"message": "Hello from Luminiteq!"}
